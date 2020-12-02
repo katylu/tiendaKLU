e447b39ef452a90911556486415fc30751a3da9a
Katiana Leguizamón Ughelli <kughelli@uaa.edu.py>
2020-12-02 00:02:12 -0300

    primer commit

---
+++ MiTiendaLeguizamon/Controllers/SeccionesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MiTiendaLeguizamon.Models;
+
+namespace MiTiendaLeguizamon.Controllers
+{
+    public class SeccionesController : Controller
+    {
+        private TiendaLeguizamonEntities db = new TiendaLeguizamonEntities();
+
+        // GET: Secciones
+        public ActionResult Index()
+        {
+            return View(db.Seccions.ToList());
+        }
+
+        // GET: Secciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seccion seccion = db.Seccions.Find(id);
+            if (seccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seccion);
+        }
+
+        // GET: Secciones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Secciones/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Descripcion")] Seccion seccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seccions.Add(seccion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seccion);
+        }
+
+        // GET: Secciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seccion seccion = db.Seccions.Find(id);
+            if (seccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seccion);
+        }
+
+        // POST: Secciones/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Descripcion")] Seccion seccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seccion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seccion);
+        }
+
+        // GET: Secciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seccion seccion = db.Seccions.Find(id);
+            if (seccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seccion);
+        }
+
+        // POST: Secciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seccion seccion = db.Seccions.Find(id);
+            db.Seccions.Remove(seccion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MiTiendaLeguizamon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiTiendaLeguizamon.Startup))]
+namespace MiTiendaLeguizamon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
